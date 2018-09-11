df8f4fda57c6f7027c33f2dddf128da442c653a2
Jose9820 <pliegogomezj@gmail.com>
2018-09-11 15:51:11 -0500

    segunda version: estructuras y memoria dinamica
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~

--- memoria_dinamica.cpp
+++ memoria_dinamica.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 
-
 using namespace std;
 
+struct Nodo{
+	int val;
+	struct Nodo *ptr;
+}*nodo,*inicio;
+
 int main(){
- 	int N;
- 	int *var;
- 	var=new int[N];
- 	cout << "Ingrese cantidad de datos: ";
- 	cin >> N;
-  	int arreglo_N[N];
- 	for (int i=0; i < N; i++){
- 		cout << "Ingrese el dato " << i+1 << ": ";
- 		cin >> arreglo_N[i];
- 	}
- 	for (int i=0; i<N; i++){
- 		cout << "\nValor " << i+1 << ": " << arreglo_N[i];
-	 }
- 	delete[] var;
+ 	nodo= new struct Nodo;
+ 	inicio=nodo;
+ 	inicio->ptr=NULL;
+ 	inicio->val=10;
+ 	cout << "primer valor: " << inicio->val << endl;
+ 	
+ 	nodo=new struct Nodo;
+ 	inicio->ptr=nodo;
+ 	inicio->ptr->ptr=NULL;
+ 	inicio->ptr->val=20;
+ 	cout << "segundo valor: " << inicio->ptr->val << endl;
 }
